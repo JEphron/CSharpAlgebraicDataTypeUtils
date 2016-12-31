01f0d1ee1276cae9b95386e31ff3c5ed6b4b91cb
JEphron <jephron63@gmail.com>
2016-12-30 22:34:31 -0500

    Use var everywhere

--- Src/Tests/TestMaybe.cs
+++ Src/Tests/TestMaybe.cs
@@ -10,70 +10,70 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestNonePrimitiveIsNone()
         {
-            Maybe<int> maybeInt = new Maybe<int>();
+            var maybeInt = new Maybe<int>();
             Assert.IsTrue(maybeInt.IsNone());
         }
 
         [Test]
         public void TestNonePrimativeIsNotSome()
         {
-            Maybe<int> maybeInt = new Maybe<int>();
+            var maybeInt = new Maybe<int>();
             Assert.IsFalse(maybeInt.IsSome());
         }
 
         [Test]
         public void TestSomePrimativeIsSome()
         {
-            Maybe<int> maybeInt = new Maybe<int>(1);
+            var maybeInt = new Maybe<int>(1);
             Assert.IsTrue(maybeInt.IsSome());
         }
 
         [Test]
         public void TestSomePrimativeIsNotNone()
         {
-            Maybe<int> maybeInt = new Maybe<int>(1);
+            var maybeInt = new Maybe<int>(1);
             Assert.IsFalse(maybeInt.IsNone());
         }
 
         [Test]
         public void TestNoneObjectIsNone()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>();
+            var maybeList = new Maybe<List<int>>();
             Assert.IsTrue(maybeList.IsNone());
         }
 
         [Test]
         public void TestNoneObjectIsNotSome()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>();
+            var maybeList = new Maybe<List<int>>();
             Assert.IsFalse(maybeList.IsSome());
         }
 
         [Test]
         public void TestSomeObjectIsSome()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
+            var maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
             Assert.IsTrue(maybeList.IsSome());
         }
 
         [Test]
         public void TestSomeObjectIsNotNone()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
+            var maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
             Assert.IsFalse(maybeList.IsNone());
         }
 
         [Test]
         public void TestNullIsNone()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>(null);
+            var maybeList = new Maybe<List<int>>(null);
             Assert.IsTrue(maybeList.IsNone());
         }
 
         [Test]
         public void TestMatchNonePrimativeTriggersNoneHandler()
         {
-            Maybe<int> maybeInt = new Maybe<int>();
+            var maybeInt = new Maybe<int>();
             maybeInt.Match(some: i => Assert.Fail("[some] handler should not be called"), none: Assert.Pass);
             Assert.Fail("Match(...) should execute immediately");
         }
@@ -81,7 +81,7 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMatchSomePrimativeTriggersSomeHandler()
         {
-            Maybe<int> maybeInt = new Maybe<int>(1);
+            var maybeInt = new Maybe<int>(1);
             maybeInt.Match(some: i => Assert.Pass(), none: () => Assert.Fail("[none] handler should not be called"));
             Assert.Fail("Match(...) should execute immediately");
         }
@@ -89,25 +89,25 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMatchSomePrimativeReturnsSome()
         {
-            Maybe<int> maybeInt = new Maybe<int>(1);
+            var maybeInt = new Maybe<int>(1);
             Assert.That(maybeInt.IsSome);
-            int match = maybeInt.Match(some: i => 1, none: () => 0);
+            var match = maybeInt.Match(some: i => 1, none: () => 0);
             Assert.That(match, Is.EqualTo(1));
         }
 
         [Test]
         public void TestMatchNonePrimativeReturnsNone()
         {
-            Maybe<int> maybeInt = new Maybe<int>();
+            var maybeInt = new Maybe<int>();
             Assert.That(maybeInt.IsNone);
-            int match = maybeInt.Match(some: i => 1, none: () => 0);
+            var match = maybeInt.Match(some: i => 1, none: () => 0);
             Assert.That(match, Is.EqualTo(0));
         }
 
         [Test]
         public void TestMatchNoneObjectTriggersNoneHandlerImmediately()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>();
+            var maybeList = new Maybe<List<int>>();
             maybeList.Match(some: i => Assert.Fail("[some] handler should not be called"), none: Assert.Pass);
             Assert.Fail("match should execute immediately");
         }
@@ -115,7 +115,7 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMatchSomeObjectTriggersSomeHandlerImmediately()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>();
+            var maybeList = new Maybe<List<int>>();
             maybeList.Match(some: i => Assert.Fail("[some] handler should not be called"), none: Assert.Pass);
             Assert.Fail("match should execute immediately");
         }
@@ -123,7 +123,7 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMatchSomeObjectReturnsSome()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
+            var maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
             var list1 = new List<string>() { "a", "b"};
             var list2 = new List<string>() {"1", "2"};
             var match = maybeList.Match(some: i => list1, none: () => list2);
@@ -133,7 +133,7 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMatchNoneObjectReturnsNone()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>();
+            var maybeList = new Maybe<List<int>>();
             var list1 = new List<string> { "a", "b" };
             var list2 = new List<string> { "1", "2" };
             var match = maybeList.Match(some: i => list1, none: () => list2);
@@ -144,61 +144,61 @@ namespace JME.UnionTypes.Tests
         public void TestMatchSomeObjectUsingAFunctionForSomeAndAValueForNoneReturnsTheResultOfTheFunction()
         {
             var list = new List<int> { 1, 2, 3 };
-            Maybe<List<int>> maybeList = new Maybe<List<int>>(list);
+            var maybeList = new Maybe<List<int>>(list);
             const string noneValue = "none";
             const string someValue = "some";
             Func<List<int>, string> someFunction = (lst) => someValue;
-            string match = maybeList.Match(some: someFunction, none: noneValue);
+            var match = maybeList.Match(some: someFunction, none: noneValue);
             Assert.That(match, Is.EqualTo(someFunction(list)));
         }
 
         [Test]
         public void TestMatchSomeObjectUsingAValueForSomeAndAFunctionForNoneReturnsTheValue()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>(new List<int> { 1, 2, 3 });
+            var maybeList = new Maybe<List<int>>(new List<int> { 1, 2, 3 });
             const string someValue = "some";
             Func<string> noneFunction = () =>
             {
                 Assert.Fail("[none] handler should not be called");
                 return "none";
             };
-            string match = maybeList.Match(some: someValue, none: noneFunction);
+            var match = maybeList.Match(some: someValue, none: noneFunction);
             Assert.That(match, Is.EqualTo(someValue));
         }
 
         [Test]
         public void TestMatchNoneObjectUsingAValueForSomeAndAFunctionForNoneReturnsTheResultOfTheFunction()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>();
+            var maybeList = new Maybe<List<int>>();
             const string someValue = "some";
             const string noneValue = "none";
             Func<string> noneFunction = () => noneValue;
-            string match = maybeList.Match(some: someValue, none: noneFunction);
+            var match = maybeList.Match(some: someValue, none: noneFunction);
             Assert.That(match, Is.EqualTo(noneFunction()));
         }
 
         [Test]
         public void TestMatchNoneObjectUsingAFunctionForSomeAndAValueForNoneReturnsTheValue()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>();
+            var maybeList = new Maybe<List<int>>();
             const string noneValue = "none";
             Func<List<int>, string> someFunction = lst =>
             {
                 Assert.Fail("[some] handler should not be called");
                 return "some";
             };
-            string match = maybeList.Match(some: someFunction, none: noneValue);
+            var match = maybeList.Match(some: someFunction, none: noneValue);
             Assert.That(match, Is.EqualTo(noneValue));
         }
 
         [Test]
         public void TestMatchSomeObjectUsingAFunctionForSomeAndAValueForNoneCallsTheFunctionImmediately()
         {
-            Maybe<List<int>> maybeList = new Maybe<List<int>>(new List<int> { 1, 2, 3 });
+            var maybeList = new Maybe<List<int>>(new List<int> { 1, 2, 3 });
             maybeList.Match(some: list => Assert.Pass(), none: () => Assert.Fail("[none] handler should not be called"));
             Assert.Fail("match should execute immediately");
         }
 
-        // todo: finish the tests
+
     }
 }
