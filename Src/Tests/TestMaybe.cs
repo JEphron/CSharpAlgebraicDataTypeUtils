b2794129bc3458e5bacd5c3e01fc90c976efd79d
JEphron <jephron63@gmail.com>
2017-01-01 12:00:19 -0500

    More tests

--- Src/Tests/TestMaybe.cs
+++ Src/Tests/TestMaybe.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using NUnit.Framework;
 
 namespace JME.UnionTypes.Tests
@@ -135,7 +134,7 @@ namespace JME.UnionTypes.Tests
         public void TestMatchSomeObjectReturnsSome()
         {
             var maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
-            var list1 = new List<string>() { "a", "b"};
+            var list1 = new List<string>() {"a", "b"};
             var list2 = new List<string>() {"1", "2"};
             var match = maybeList.Match(some: i => list1, none: () => list2);
             Assert.That(match, Is.EqualTo(list1));
@@ -145,8 +144,8 @@ namespace JME.UnionTypes.Tests
         public void TestMatchNoneObjectReturnsNone()
         {
             var maybeList = new Maybe<List<int>>();
-            var list1 = new List<string> { "a", "b" };
-            var list2 = new List<string> { "1", "2" };
+            var list1 = new List<string> {"a", "b"};
+            var list2 = new List<string> {"1", "2"};
             var match = maybeList.Match(some: i => list1, none: () => list2);
             Assert.That(match, Is.EqualTo(list2));
         }
@@ -154,7 +153,7 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMatchSomeObjectUsingAFunctionForSomeAndAValueForNoneReturnsTheResultOfTheFunction()
         {
-            var list = new List<int> { 1, 2, 3 };
+            var list = new List<int> {1, 2, 3};
             var maybeList = new Maybe<List<int>>(list);
             const string noneValue = "none";
             const string someValue = "some";
@@ -166,7 +165,7 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMatchSomeObjectUsingAValueForSomeAndAFunctionForNoneReturnsTheValue()
         {
-            var maybeList = new Maybe<List<int>>(new List<int> { 1, 2, 3 });
+            var maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
             const string someValue = "some";
             Func<string> noneFunction = () =>
             {
@@ -205,14 +204,14 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMatchSomeObjectUsingAFunctionForSomeAndAValueForNoneCallsTheFunctionImmediately()
         {
-            var maybeList = new Maybe<List<int>>(new List<int> { 1, 2, 3 });
+            var maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
             maybeList.Match(some: list => Assert.Pass(), none: Assert.Fail);
         }
 
         [Test]
         public void TestMatchSomeObjectUsingAValueForSomeAndAValueForNoneReturnsTheValueForSome()
         {
-            var maybeList = new Maybe<List<int>>(new List<int> {1,2,3});
+            var maybeList = new Maybe<List<int>>(new List<int> {1, 2, 3});
             const string someValue = "some";
             const string noneValue = "none";
             var match = maybeList.Match(some: someValue, none: noneValue);
@@ -232,7 +231,7 @@ namespace JME.UnionTypes.Tests
         [Test]
         public void TestMapReturnsSomeWhenCalledOnSome()
         {
-            var maybeList = new Maybe<List<string>>(new List<string>{"a", "b"});
+            var maybeList = new Maybe<List<string>>(new List<string> {"a", "b"});
             var mapResult = maybeList.Map(list => list[0]);
             Assert.That(mapResult.IsSome);
         }
@@ -249,7 +248,7 @@ namespace JME.UnionTypes.Tests
         public void TestOkayOrReturnsOkayWhenCalledOnSome()
         {
             var errorValue = "error";
-            var someValue = new List<string>{"foo", "bar"};
+            var someValue = new List<string> {"foo", "bar"};
             var maybeList = new Maybe<List<string>>(someValue);
             var okayOr = maybeList.OkayOr(errorValue);
             okayOr.Match(list => Assert.Pass(), err => Assert.Fail());
@@ -262,6 +261,162 @@ namespace JME.UnionTypes.Tests
             var maybeList = new Maybe<List<string>>();
             var okayOr = maybeList.OkayOr(errorValue);
             okayOr.Match(list => Assert.Fail(), err => Assert.Pass());
+        }
+
+        [Test]
+        public void TestValueOrReturnsSomeWhenCalledOnSomeAndPassedAValue()
+        {
+            const string some = "foo";
+            const string alternative = "bar";
+            var maybeString = new Maybe<string>(some);
+            var result = maybeString.ValueOr(alternative);
+            Assert.That(result, Is.EqualTo(some));
+        }
+
+        [Test]
+        public void TestValueOrReturnsParameterWhenCalledOnNoneAndPassedAValue()
+        {
+            const string alternative = "bar";
+            var maybeString = new Maybe<string>();
+            var result = maybeString.ValueOr(alternative);
+            Assert.That(result, Is.EqualTo(alternative));
+        }
+
+        [Test]
+        public void TestValueOrReturnsSomeWhenCalledOnSomeAndPassedAFunction()
+        {
+            const string some = "foo";
+            var maybeString = new Maybe<string>(some);
+            var result = maybeString.ValueOr(() => "abc");
+            Assert.That(result, Is.EqualTo(some));
+        }
+
+        [Test]
+        public void TestValueOrReturnsParameterWhenCalledOnNoneAndPassedAFunction()
+        {
+            Func<string> alternative = () => "bar";
+            var maybeString = new Maybe<string>();
+            var result = maybeString.ValueOr(alternative);
+            Assert.That(result, Is.EqualTo(alternative()));
+        }
+
+        [Test]
+        public void TestOrDefaultReturnsSomeWhenCalledOnSome()
+        {
+            const string some = "foo";
+            var maybeString = new Maybe<string>(some);
+            var result = maybeString.OrDefault();
+            Assert.That(result, Is.EqualTo(some));
+        }
+
+        [Test]
+        public void TestOrDefaultReturnsDefaultWhenCalledOnNoneObject()
+        {
+            var maybeString = new Maybe<string>();
+            var result = maybeString.OrDefault();
+            Assert.That(result, Is.EqualTo(default(string)));
+        }
+
+        [Test]
+        public void TestOrDefaultReturnsDefaultWhenCalledOnNonePrimative()
+        {
+            var maybeInt = new Maybe<int>();
+            var result = maybeInt.OrDefault();
+            Assert.That(result, Is.EqualTo(default(int)));
+        }
+
+        [Test]
+        public void TestAsNoneReturnsNoneWhenCalledOnSome()
+        {
+            var maybeInt = new Maybe<int>(99);
+            Assert.That(maybeInt.AsNone().IsNone);
+        }
+
+        [Test]
+        public void TestAsNoneReturnsNoneWhenCalledOnNone()
+        {
+            var maybeInt = new Maybe<int>();
+            Assert.That(maybeInt.AsNone().IsNone);
+        }
+
+        [Test]
+        public void TestAsSomeReturnsSomeWhenCalledOnSome()
+        {
+            var maybeInt = new Maybe<int>(99);
+            var result = maybeInt.AsSome(66).OrDefault();
+            Assert.That(result, Is.EqualTo(66));
+        }
+
+        [Test]
+        public void TestAsSomeReturnsSomeWhenCalledOnNone()
+        {
+            var maybeInt = new Maybe<int>();
+            var result = maybeInt.AsSome(66).OrDefault();
+            Assert.That(result, Is.EqualTo(66));
+        }
+
+        [Test]
+        public void TestAndReturnsNoneWhenCalledOnNone()
+        {
+            var maybeString = new Maybe<string>();
+            var result = maybeString.And(new Maybe<string>("foo"));
+            Assert.That(result.IsNone);
+        }
+
+        [Test]
+        public void TestAndReturnsArgumentWhenCalledOnSome()
+        {
+            var maybeString = new Maybe<string>("foo");
+            var result = maybeString.And(new Maybe<string>("bar"));
+            Assert.That(result.OrDefault(), Is.EqualTo("bar"));
+        }
+
+        [Test]
+        public void TestOrReturnsSelfWhenCalledOnSome()
+        {
+            var maybeString = new Maybe<string>("foo");
+            var result = maybeString.Or(new Maybe<string>("bar"));
+            Assert.That(result.OrDefault(), Is.EqualTo("foo"));
+        }
+
+        [Test]
+        public void TestOrReturnsArgumentWhenCalledOnNone()
+        {
+            var maybeString = new Maybe<string>();
+            var result = maybeString.Or(new Maybe<string>("bar"));
+            Assert.That(result.OrDefault(), Is.EqualTo("bar"));
+        }
+
+        [Test]
+        public void TestAndThenReturnsTheValueOfTheFunctionWhenCalledOnSome()
+        {
+            var maybeString = new Maybe<string>("foo");
+            var result = maybeString.AndThen(s => new Maybe<string>(s + "bar"));
+            Assert.That(result.OrDefault(), Is.EqualTo("foobar"));
+        }
+
+        [Test]
+        public void TestAndThenReturnsNoneWhenCalledOnNone()
+        {
+            var maybeString = new Maybe<string>();
+            var result = maybeString.AndThen(s => new Maybe<string>("bar"));
+            Assert.That(result.IsNone);
+        }
+
+        [Test]
+        public void TestOrElseReturnsSelfWhenCalledOnSome()
+        {
+            var maybeString = new Maybe<string>("foo");
+            var result = maybeString.OrElse(() => new Maybe<string>("bar"));
+            Assert.That(result.OrDefault(), Is.EqualTo("foo"));
+        }
+
+        [Test]
+        public void TestOrElseReturnsTheValueOfTheFunctionWhenCalledOnNone()
+        {
+            var maybeString = new Maybe<string>();
+            var result = maybeString.OrElse(() => new Maybe<string>("bar"));
+            Assert.That(result.OrDefault(), Is.EqualTo("bar"));
         }
     }
 }
