936212561be87837529f7f23be057c8bd1dd3a26
JEphron <jephron63@gmail.com>
2017-01-02 13:03:15 -0500

    Fix formatting in Union3

--- Src/Union3.cs
+++ Src/Union3.cs
@@ -25,17 +25,19 @@ namespace JME.UnionTypes
 
         public void Match(Action<T1> f1, Action<T2> f2, Action<T3> f3)
         {
-            _t1.Match(some: f1, none: () =>
-            {
-                _t2.Match(some: f2, none: () =>
+            _t1.Match(
+                some: f1,
+                none: () =>
                 {
-                    _t3.Match(some: f3, none: () =>
-                    {
-
-                    });
+                    _t2.Match(
+                        some: f2,
+                        none: () =>
+                        {
+                            _t3.Match(
+                                some: f3,
+                                none: () => { throw new Exception("No match in Union3"); });
+                        });
                 });
-            });
-
         }
 
         public TResult Match<TResult>(Func<T1, TResult> f1, Func<T2, TResult> f2, Func<T3, TResult> f3)
